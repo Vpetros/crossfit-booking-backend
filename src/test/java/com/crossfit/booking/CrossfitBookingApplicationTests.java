4778d13feeca0d09aeed9de70122a78fe8f2f9df
Vpetros <rawkudodes@hotmail.com>
2026-01-27 11:08:48 +0200

    fix: changed MongoDB config string

--- src/test/java/com/crossfit/booking/CrossfitBookingApplicationTests.java
+++ src/test/java/com/crossfit/booking/CrossfitBookingApplicationTests.java
@@ -2,6 +2,7 @@ package com.crossfit.booking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class CrossfitBookingApplicationTests {
